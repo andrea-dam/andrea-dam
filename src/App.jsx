c20c72dc51fe6ba43e4ce2d895bd28367ad20ea5
Andrea Damiani <andrea.dam90@gmail.com>
2024-02-05 22:32:07 +0100

    Barra laterale responsive e prima stesura intro

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,20 @@ import pagine from "./data/pagine.json";
 export default function App() {
 	return (
 		<>
-			<div className="container mx-auto h-[100dvh] overflow-y-scroll scroll-smooth px-44">
+			<div className="container mx-auto h-[100dvh] px-20 lg:px-44">
 				<section
 					id="header"
 					className="mt-20 min-h-[100dvh] scroll-my-20">
-					<header className="mb-20 flex flex-col">
+					<header className="mb-40 flex flex-col">
 						<h1 className="font-display text-8xl font-medium tracking-tighter">
 							Andrea Damiani
 						</h1>
 						<h2 className="text-7xl">Web Developer</h2>
 					</header>
 					<p className="text-end text-2xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Asperiores nisi molestiae earum harum ducimus at, dolor
-						sequi perferendis minus ipsam? Impedit incidunt quae
-						sunt quasi quos architecto cum cumque aliquid?
+						Sono uno sviluppatore web con esperienza in JavaScript e
+						PHP. Mi trovo particolarmente a mio agio nello sviluppo
+						con Vue.js e ho da poco cominciato a studiare React.
 					</p>
 				</section>
 
@@ -28,9 +27,9 @@ export default function App() {
 					<header className="mb-10">
 						<h3 className="text-4xl">Progetti</h3>
 					</header>
-					<div className="grid grid-cols-3 gap-x-3 gap-y-9">
+					<ul className="grid grid-cols-1 gap-x-3 gap-y-9 md:grid-cols-2 lg:grid-cols-3">
 						{pagine.map((progetto) => (
-							<div
+							<li
 								key={progetto.titolo}
 								className="flex flex-col gap-1.5">
 								<div className="h-max">
@@ -47,9 +46,9 @@ export default function App() {
 										Visita
 									</a>
 								</div>
-							</div>
+							</li>
 						))}
-					</div>
+					</ul>
 				</section>
 			</div>
 
--- src/components/SidebarLine.jsx
+++ src/components/SidebarLine.jsx
@@ -7,10 +7,15 @@ import FluentCertificate16Regular from "./icons/FluentCertificate16Regular";
 
 export default function SidebarLine() {
 	return (
-		<div className="fixed inset-y-0 left-10 flex h-full w-[3px] flex-col items-center justify-center gap-3 bg-neutral-950">
-			<LateralIconWrapper icon={PhUser()} href="#header" />
-			<LateralIconWrapper icon={IconParkOutlinePage()} href="#projects" />
-			<LateralIconWrapper icon={FluentCertificate16Regular()} />
+		<div className="fixed top-0 flex w-full items-center bg-neutral-50 py-7 lg:left-10 lg:h-full lg:w-auto lg:py-0">
+			<div className=" flex h-[3px]  w-full items-center justify-center gap-3 bg-neutral-950 lg:inset-y-0 lg:left-10 lg:h-full lg:w-[3px] lg:flex-col">
+				<LateralIconWrapper icon={PhUser()} href="#header" />
+				<LateralIconWrapper
+					icon={IconParkOutlinePage()}
+					href="#projects"
+				/>
+				<LateralIconWrapper icon={FluentCertificate16Regular()} />
+			</div>
 		</div>
 	);
 }
